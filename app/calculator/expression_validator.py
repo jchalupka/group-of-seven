fd5a2e4cc71d9e56d19845137993350043844b39
hongr07 <hongr@mail.uoguelph.ca>
2016-11-17 16:10:15 -0500

    Added mod function to expression validator allowing program to process it

--- app/calculator/expression_validator.py
+++ app/calculator/expression_validator.py
@@ -140,7 +140,7 @@ def valid_arithmetic_expression(expression):
                     stack.insert(0, token[1:])
                 token = '-'
 
-            if re.match('^[\+\-\/\*\^]$',token):
+            if re.match('^[\+\-\/\*\^\%]$',token):
                 state = 0
             elif re.match('^\)$',token):
                 state = 1
