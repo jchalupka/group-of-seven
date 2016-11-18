4a78361f27bf11fbd5ee254277c6bd47e73f6df3
Roli Jule <rjulebla@mail.uoguelph.ca>
2016-11-18 15:55:18 -0500

    updated format output

--- app/calculator/command_line.py
+++ app/calculator/command_line.py
@@ -1,18 +1,22 @@
 import processing
 
+columns = "{0:30}{1:5}{2:30}"
 
 def evaluate_from_file(filename):
     try:
         with open(filename, "r") as f:
+            print columns.format("Expression", " | ", "Answer")
+            print "------------------------------------------------------------------------------"
             for expression in f:
                 expression = expression.replace("\n", "")
                 result = processing.evaluate_expression(expression, None)
                 try:
                     float(result)
-                    print expression + " = " + result
+                    print columns.format(expression, " = " , result)
                 except ValueError:
-                    print result
+                    print columns.format(expression, " = " , result)
                 except TypeError:
-                    print "Cannot graph expressions from file"
+                    print columns.format(expression, " = " , "Cannot graph expressions from file")
+            print "------------------------------------------------------------------------------"
     except IOError:
         print "Could not find file"
